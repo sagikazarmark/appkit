6698eb8eadf8bb2503a59118a9dec9a8c85c3c7f
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-11-13 01:33:28 +0100

    build: improve nix matrix build
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -13,18 +13,17 @@
         buildDeps = with pkgs; [ git go gnumake ];
         devDeps = with pkgs; buildDeps ++ [ golangci-lint gotestsum ];
 
-        generateGoEnv = go:
-          pkgs.buildEnv {
-            name = "go" + go.version;
-            paths = (pkgs.lib.remove pkgs.go devDeps) ++ [ go ];
+        goShell = go:
+          pkgs.mkShell {
+            buildInputs = (pkgs.lib.remove pkgs.go devDeps) ++ [ go ];
           };
       in {
         devShell = pkgs.mkShell {
           buildInputs = devDeps;
         };
 
-        packages.go1_15 = generateGoEnv pkgs.go_1_15;
-        packages.go1_16 = generateGoEnv pkgs.go_1_16;
-        packages.go1_17 = generateGoEnv pkgs.go_1_17;
+        devShells.go1_15 = goShell pkgs.go_1_15;
+        devShells.go1_16 = goShell pkgs.go_1_16;
+        devShells.go1_17 = goShell pkgs.go_1_17;
       });
 }
